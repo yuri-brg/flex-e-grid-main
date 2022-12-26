b19391d98cb4e79d37a52c27e0ab63b89f6ccdfd
Yuri <yuri_lucas7@hotmail.com>
2022-12-26 20:22:21 -0300

    Alura

---
+++ flex-e-grid-main/index.js
@@ -0,0 +1,5 @@
+const botaoMenu = document.querySelector('.cabecalho_menu')
+const menu = document.querySelector('.menu-lateral')
+
+botaoMenu.addEventListener('click', () => { 
+    menu.classList.toggle('menu-lateral--ativo') })
